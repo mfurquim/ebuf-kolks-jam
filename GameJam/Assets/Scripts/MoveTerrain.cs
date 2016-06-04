ee3a959450a2a7079e0cb5022fab9cda1587e100
MasterCaio <caiofelipe147@gmail.com>
2016-06-04 14:18:37 -0300

    Criando script para repetir cenário

---
+++ GameJam/Assets/Scripts/MoveTerrain.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTerrain : MonoBehaviour {
+
+	private Material material;
+	public float speed;
+	private float offset;
+
+	void Start () {
+		material = GetComponent<Renderer>().material;
+	}
+
+	void Update () {
+		offset += (speed * Time.deltaTime);
+		material.SetTextureOffset("_MainText", new Vector2(offset,0));
+	}
+}
